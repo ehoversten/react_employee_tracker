f16a503746ef8a39ad1cb0ac2754e7768a2451ca
ehoversten <ehoversten@gmail.com>
2020-03-02 22:27:06 -0500

    added Nav component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <div className="navigation">
+            <div className="nav-container">
+                <h1>Welcome to the Employee Manager</h1>
+                <p>Search by last name to filter results</p>
+            </div>
+        </div>
+    )
+}
+
+
+export default Nav;
