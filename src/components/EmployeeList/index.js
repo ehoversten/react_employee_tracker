a11bea7772f3ab8fb883bfe9bdaec6e23b03ac62
ehoversten <ehoversten@gmail.com>
2020-03-02 23:37:22 -0500

    added EmployeeList component, displaying API user data

--- src/components/EmployeeList/index.js
+++ src/components/EmployeeList/index.js
@@ -7,7 +7,12 @@ function EmployeeList(props) {
         <div className="employee-list">
             <ul className="list-group">
                 {props.employees.map(user => (
-                    <li key={user.id}>Name: {user.name}</li>
+                    <div key={user.id}>
+                        <p>Name: {user.name}</p>
+                        <p>Email: {user.email}</p>
+                        <p>Company: {user.company.name}</p>
+                        <br/>
+                    </div>
                 ))}
             </ul>
         </div>
