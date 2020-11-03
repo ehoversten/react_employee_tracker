60db3cfb9af48d30c57e39f43928db609a316065
ehoversten <ehoversten@gmail.com>
2020-11-02 21:05:51 -0500

    Updates

---
+++ precache-manifest.18744de3dddc5a461d970d141f48381b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5de8ff8760f27738aefa140a6125f5d",
+    "url": "/react_employee_tracker/index.html"
+  },
+  {
+    "revision": "f8b51e8a3ba542d6dde0",
+    "url": "/react_employee_tracker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2c8fd85169403a35d5d8",
+    "url": "/react_employee_tracker/static/js/2.c7688d26.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react_employee_tracker/static/js/2.c7688d26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8b51e8a3ba542d6dde0",
+    "url": "/react_employee_tracker/static/js/main.050eed68.chunk.js"
+  },
+  {
+    "revision": "9cf21e36f507d45a51a6",
+    "url": "/react_employee_tracker/static/js/runtime-main.2a9f8eb6.js"
+  }
+]);
