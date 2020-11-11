ad460bc5aadae6ba02361be7357347ba87261401
ehoversten <ehoversten@gmail.com>
2020-11-10 21:53:41 -0500

    added bootstrap library, adjusted navbar class

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,16 +3,30 @@ import { NavLink } from 'react-router-dom';
 
 function Nav() {
     return (
-      <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">Employee Directory</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="/">
+          Employee Directory
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-                <NavLink to="/" className="nav-item nav-link">Home</NavLink>
-                <NavLink to="/employees" className="nav-item nav-link">Directory</NavLink>
-            </ul>
+          <ul className="navbar-nav">
+            <NavLink to="/" className="nav-item nav-link">
+              Home
+            </NavLink>
+            <NavLink to="/employees" className="nav-item nav-link">
+              Directory
+            </NavLink>
+          </ul>
         </div>
       </nav>
     );
